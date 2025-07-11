9a4773bd57e76b50fe73922dddfb77b16a2afeea
liewgzh <liewgzh@gmail.com>
2025-07-11 12:20:20 +1000

    commented out managed node 2 from outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,9 +2,9 @@ output "control_node_public_ip" {
   value = aws_instance.control_node.public_ip
 }
 
-output "managed_node_1_private_ip" {
-  value = aws_instance.managed_node_1.private_ip
-}
+# output "managed_node_1_private_ip" {
+#   value = aws_instance.managed_node_1.private_ip
+# }
 
 output "managed_node_2_private_ip" {
   value = aws_instance.managed_node_2.private_ip
